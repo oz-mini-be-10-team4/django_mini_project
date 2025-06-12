d4915cd83f4fb853c5d2b82d3feeb032a73d2032
choismjames23 <choism23@naver.com>
2025-06-12 16:55:43 +0900

    static root

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,6 +91,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
